89a3ad0302d8ac0cf47dfc451e50ff1a077f61e5
Somk1ng <148505779+Somk1ng@users.noreply.github.com>
2024-03-01 17:55:25 +0700

    Saat lahir!

---
+++ index.js
@@ -0,0 +1,12 @@
+'use strict'
+const dateformat = require('dateformat')
+
+function yesterdayone(timestamp, format) {
+	const date = new Date(timestamp)
+
+	date.setDate(date.getDate() - 1)
+
+	return dateformat(date, format)
+}
+
+module.exports = yesterdayone
